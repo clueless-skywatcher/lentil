2ca145289bced87f57ab699cd2c78a57de904ffe
clueless-skywatcher <somichat@gmail.com>
2023-10-12 00:35:05 +0530

    Changed name of sample. Samples will be moved to tests later

--- src/main/java/com/github/cluelessskywatcher/lentil/LentilMain.java
+++ src/main/java/com/github/cluelessskywatcher/lentil/LentilMain.java
@@ -3,7 +3,8 @@ package com.github.cluelessskywatcher.lentil;
 import com.github.cluelessskywatcher.lentil.report.LentilRunner;
 
 public class LentilMain {
-    private static final String FILE_PATH = "src/main/java/com/github/cluelessskywatcher/lentil/samples/Somefile.java";
+    private static final String FILE_PATH = 
+        "src/main/java/com/github/cluelessskywatcher/lentil/samples/AsteriskImportExample.java";
     public static void main(String[] args) throws Exception{
         LentilRunner runner = new LentilRunner(FILE_PATH);
         for (String report : runner.getReports()){
---
+++ src/main/java/com/github/cluelessskywatcher/lentil/samples/AsteriskImportExample.java
@@ -0,0 +1,9 @@
+package com.github.cluelessskywatcher.lentil.samples;
+
+import java.util.*;
+import java.awt.color.CMMException;
+import java.awt.geom.*;
+
+public class AsteriskImportExample {
+    int x = 5;
+}
