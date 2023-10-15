42847cecaca49582e9eb05340c4f9fe18414fdee
clueless-skywatcher <somichat@gmail.com>
2023-10-15 12:54:46 +0530

    Added check: AnnotationOnSameLineChecker: Check whether annotations are on same line

--- src/main/java/com/github/cluelessskywatcher/lentil/checks/annotations/AnnotationsOnSameLineChecker.java
+++ src/main/java/com/github/cluelessskywatcher/lentil/checks/annotations/AnnotationsOnSameLineChecker.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.tuple.ImmutablePair;
 import com.github.cluelessskywatcher.lentil.checks.AbstractChecker;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 
@@ -15,7 +16,7 @@ public class AnnotationsOnSameLineChecker extends AbstractChecker {
     public AnnotationsOnSameLineChecker(){
         issueCode = "A0001";
         messageFormat = "Annotation @{0} present on the same line as declaration {1}";
-        description = "Annotations should preferably be used on a different line as their targets";
+        description = "Annotations should be used on a different line as their targets";
     }
 
     @Override
@@ -65,6 +66,30 @@ public class AnnotationsOnSameLineChecker extends AbstractChecker {
                 varDecl.getAnnotations().forEach(
                     anno -> {
                         varDecl.getVariables().forEach(
+                            var -> {
+                                if (var.getName().getBegin().get().line 
+                                    == anno.getName().getBegin().get().line){
+                                    reports.add(new ImmutablePair<Integer,String>(
+                                        var.getBegin().get().line,
+                                        MessageFormat.format(
+                                            messageFormat,
+                                            anno.getNameAsString(),
+                                            var.getNameAsString()
+                                        )
+                                    ));
+                                }
+                            }
+                        );
+                    }
+                );
+            }
+        );
+
+        cu.findAll(FieldDeclaration.class).forEach(
+            fieldDecl -> {
+                fieldDecl.getAnnotations().forEach(
+                    anno -> {
+                        fieldDecl.getVariables().forEach(
                             var -> {
                                 if (var.getName().getBegin().get().line 
                                     == anno.getName().getBegin().get().line){
