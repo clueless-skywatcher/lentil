1e2d34f5d6452b16d329b8a62dee3dbb0655aa73
clueless-skywatcher <somichat@gmail.com>
2023-10-23 15:00:53 +0530

    Added check: BooleanReturnsNullChecker: Check whether a function of boolean type is returning null

---
+++ src/main/java/com/github/cluelessskywatcher/lentil/LentilQuickRunner.java
@@ -0,0 +1,26 @@
+package com.github.cluelessskywatcher.lentil;
+
+import java.text.MessageFormat;
+
+import com.github.cluelessskywatcher.lentil.report.LentilRunner;
+import com.github.cluelessskywatcher.lentil.utilities.ConfigJSONReader;
+
+public class LentilQuickRunner {
+    /*
+     * Code to run the LentilRunner on a hardcoded file
+     */
+    public static void main(String[] args) throws Exception{
+        try {
+            String filePath = "src/main/java/com/github/cluelessskywatcher/lentil/samples/BooleanReturnNullExample.java";
+            LentilRunner runner;
+            runner = new LentilRunner(filePath);
+            System.out.println(MessageFormat.format("{0} issues found", runner.getReports().size()));
+            for (String report : runner.getReports()){
+                System.out.println(report);
+            }
+        } 
+        catch (Exception e){
+            e.printStackTrace();
+        }       
+    }
+}
